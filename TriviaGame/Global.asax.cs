507a1bf103e9affc8b152636516b227d50fde84c
MayankMehta <mehta.v.mayank@gmail.com>
2016-11-20 12:17:20 -0500

    Commit

---
+++ TriviaGame/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using TriviaGame.Models;
+
+namespace TriviaGame
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            System.Data.Entity.Database.SetInitializer<TriviaDbContext>(new SampleData().Initialize());
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ TriviaGame/Models/TriviaDbContext.cs
@@ -0,0 +1,49 @@
+﻿using System.Data.Entity;
+
+namespace TriviaGame.Models
+{
+    public class TriviaDbContext : DbContext
+    {
+        private static bool _created = false;
+
+        public TriviaDbContext()
+        {
+            if (!_created)
+            {
+                _created = true;
+                //Database.EnsureCreated();
+                Database.CreateIfNotExists();
+            }
+        }
+
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }*/
+
+        protected override void OnModelCreating(DbModelBuilder builder)
+        {
+            builder.Entity<TriviaOption>()
+                .HasKey(o => new { o.QuestionId, o.Id });
+
+            /*builder.Entity<TriviaAnswer>()
+                .HasOne(a => a.TriviaOption)
+                .WithMany()
+                .HasForeignKey(a => new { a.QuestionId, a.OptionId });*/
+
+            builder.Entity<TriviaAnswer>()
+                .HasRequired(a => a.TriviaOption)
+                .WithMany()
+                .HasForeignKey(a => new { a.QuestionId, a.OptionId });
+
+            builder.Entity<TriviaQuestion>()
+                .HasMany(q => q.Options)
+                .WithRequired(o => o.TriviaQuestion);
+        }
+        
+        public DbSet<TriviaQuestion> TriviaQuestions { get; set; }
+
+        public DbSet<TriviaOption> TriviaOptions { get; set; }
+
+        public DbSet<TriviaAnswer> TriviaAnswers { get; set; }
+    }
+}
